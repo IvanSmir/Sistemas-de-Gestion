3bb9b70b10ed307dcda4510268999161c003ac16
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-04-26 20:46:00 -0400

    Created Project Structure

---
+++ client/src/pages/generales/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../app/layout'
+const index = () => {
+  return (
+    <Layout>
+        <div>index</div>
+    </Layout>
+  )
+}
+
+export default index
