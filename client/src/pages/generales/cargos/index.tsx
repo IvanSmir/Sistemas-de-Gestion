9164033f2fc8f5d2878875ba48cf00e068e25bae
Makiko <makiko.yamamoto@fiuni.edu.py>
2024-05-03 07:25:22 -0400

    hice mi tabla

---
+++ client/src/pages/generales/cargos/index.tsx
@@ -0,0 +1,13 @@
+import ListaCargo from '@/components/generales/cargo/ListaCargo'
+import Layout from '../../../app/layout'
+
+const index = () =>{
+    return(
+        <Layout>
+            <ListaCargo/>  
+        </Layout>
+    )
+
+}
+
+export default index
