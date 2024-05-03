5ca0921531fcb38c5e45350f346309be825b11fe
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-05-03 17:08:28 -0400

    Cambios de luci y de makiko

---
+++ client/src/pages/funcionarios/add/index.tsx
@@ -0,0 +1,14 @@
+import { Form } from '@/components/funcionarios/Form';
+import React from 'react'
+import '../../app/globals.css'
+import Layout from '../../../app/layout'
+const index = () => {
+  return (
+    <Layout>
+
+
+      <Form />
+    </Layout>
+  )
+}
+export default index;
--- client/src/pages/funcionarios/index.tsx
+++ client/src/pages/funcionarios/index.tsx
@@ -1,14 +1,103 @@
+'use client'
 import { Form } from '@/components/funcionarios/Form';
-import React from 'react'
+import React, { useState } from 'react';
+import { BtnAdd } from '@/components/funcionarios/BtnAdd';
+import { BtnEdit } from '@/components/funcionarios/BtnEdit';
+import { BtnDelete } from '@/components/funcionarios/BtnDelete';
+
 import '../../app/globals.css'
 import Layout from '../../app/layout'
-const index = () => {
-  return (
-    <Layout>
+const Index = () => {
+    const funcionarios = [
+        {
+            ruc: "1045555-3",
+            nombre: 'Adrian Grahl',
+            cargo: "Gerente",
+            fechaNac: '20/01/2000',
+        },
+        {
+            ruc: "1045555-3",
+            nombre: 'Adrian Grahl',
+            cargo: "Gerente",
+            fechaNac: '20/01/2000',
 
 
-        <Form/>
-    </Layout>
-  )
+        },
+        {
+            ruc: "1045555-3",
+            nombre: 'Adrian Grahl',
+            cargo: "Gerente",
+            fechaNac: '20/01/2000',
+        },
+        {
+            ruc: "1045555-3",
+            nombre: 'Adrian Grahl',
+            cargo: "Gerente",
+            fechaNac: '20/01/2000',
+        },
+        {
+            ruc: "1045555-3",
+            nombre: 'Adrian Grahl',
+            cargo: "Gerente",
+            fechaNac: '20/01/2000',
+        },]
+
+    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+    const [rucFuncionarioEditado, setRucFuncionarioEditado] = useState(null);
+
+    const abrirFormulario = () => {
+        setMostrarFormulario(true);
+    };
+
+    const editarFuncionario = (ruc: React.SetStateAction<null>) => {
+        setRucFuncionarioEditado(ruc);
+        setMostrarFormulario(true);
+    }
+
+    return (
+        <Layout>
+
+
+            <div className="flex justify-between items-center">
+                <BtnAdd onClick={abrirFormulario} />
+            </div>
+
+            <div className="flex justify-center">
+                {mostrarFormulario && <Form />}
+            </div>
+
+            <div className="flex justify-center">
+                <table className='table-auto w-full'>
+                    <thead>
+                        <tr>
+                            <th className='px-4 py-2'>Ruc</th>
+                            <th className='px-4 py-2'>Nombre</th>
+                            <th className='px-4 py-2'>Cargo</th>
+                            <th className='px-4 py-2'>Fecha Nacimiento</th>
+                            <th className='px-4 py-2'>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {funcionarios.map((funcionario) => {
+                            return (
+                                <tr key={funcionario.ruc}>
+                                    <td className='border px-4 py-2'>{funcionario.ruc}</td>
+                                    <td className='border px-4 py-2'>{funcionario.nombre}</td>
+                                    <td className='border px-4 py-2'>{funcionario.cargo}</td>
+                                    <td className='border px-4 py-2'>{funcionario.fechaNac}</td>
+                                    <td className='border px-4 py-2'>
+                                        <div>
+
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </Layout>
+
+    )
 }
-export default index;
+export default Index;
