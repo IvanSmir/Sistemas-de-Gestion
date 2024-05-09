8804e29d7a42203f16fab01b9c514246094464b7
Gessica <adarogessica@gmail.com>
2024-05-08 23:15:48 -0400

    tabla para listar familiares incompleta

---
+++ client/src/components/familiares/List.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import {
+    TableContainer,
+    Table,
+    TableCaption,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+} from "@chakra-ui/react"; 
+
+interface Familiar {
+    name: string;
+    birthday: string;
+    relationship: string;
+}
+
+export const List: React.FC = () => {
+    const [formData, setFormData] = useState<Familiar>({
+    name: "",
+    birthday: "",
+    relationship: "",
+});
+
+    useEffect(() => {
+    
+}, []);
+
+return (
+
+<TableContainer>
+    <Table variant="simple">
+        <Thead>
+            <Tr>
+                <Th>Nombre</Th>
+                <Th>Fecha de cumpleaños</Th>
+                <Th>Parentesco</Th>
+        </Tr>
+        </Thead>
+        <Tbody>
+            <Tr>
+                <Td>{formData.name}</Td>
+                <Td>{formData.birthday}</Td>
+                <Td>{formData.relationship}</Td>
+            </Tr>
+        </Tbody>
+    </Table>
+</TableContainer>
+);
+};
---
+++ client/src/pages/familiares/index.tsx
@@ -0,0 +1,11 @@
+import { List } from '@/components/familiares/List';
+import React from 'react'
+import Layout from '../../app/layout'
+const index = () => {
+    return (
+    <Layout>
+        <List />
+    </Layout>
+)
+}
+export default index;
