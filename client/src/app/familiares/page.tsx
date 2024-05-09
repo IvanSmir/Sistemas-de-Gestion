dc8490645b43c791dd052067ea42f398e819e5ab
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-05-08 23:37:49 -0400

    uni cambios de gessica

---
+++ client/src/app/familiares/page.tsx
@@ -0,0 +1,9 @@
+import { List } from '@/components/familiares/List';
+import React from 'react'
+import Layout from '../../app/layout'
+const index = () => {
+    return (
+        <List />
+    )
+}
+export default index;
