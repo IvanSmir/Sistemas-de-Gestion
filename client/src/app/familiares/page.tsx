6220f254cd10c8170eaaff0eeea4660b689b7979
Gessica <adarogessica@gmail.com>
2024-05-09 22:54:08 -0400

    agregue iconos de editar y borrar sin funcionalidad

--- client/src/app/familiares/page.tsx
+++ client/src/app/familiares/page.tsx
@@ -1,6 +1,5 @@
 import { List } from '@/components/familiares/List';
 import React from 'react'
-import Layout from '../../app/layout'
 const index = () => {
     return (
         <List />
--- client/src/components/familiares/List.tsx
+++ client/src/components/familiares/List.tsx
@@ -1,12 +1,16 @@
-'use client'
-import React, { useEffect, useState } from "react";
+'use client';
+import React,{useState} from "react";
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import ReactPaginate from 'react-paginate';
 import {
+    Box,
+    Flex,
+    Heading,
+    Button,
     TableContainer,
     Table,
-    TableCaption,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
@@ -14,40 +18,63 @@ import {
 
 interface Familiar {
     name: string;
+    ci: string;
     birthday: string;
     relationship: string;
 }
 
 export const List: React.FC = () => {
-    const [formData, setFormData] = useState<Familiar>({
-        name: "",
-        birthday: "",
-        relationship: "",
-    });
-
-    useEffect(() => {
-
-    }, []);
-
+    const familiares: Familiar[] = [
+        {
+            name: "Juan Pérez",
+            ci: "12345678",
+            birthday: "01/01/1990",
+            relationship: "Padre",
+        },
+        {
+            name: "María Gómez",
+            ci: "87654321",
+            birthday: "15/06/1992",
+            relationship: "Madre",
+        },
+        {
+            name: "Ana Pérez",
+            ci: "45678912",
+            birthday: "20/03/2015",
+            relationship: "Hija",
+        },
+    ];
     return (
-
-        <TableContainer>
-            <Table variant="simple">
-                <Thead>
-                    <Tr>
-                        <Th>Nombre</Th>
-                        <Th>Fecha de cumpleaños</Th>
-                        <Th>Parentesco</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    <Tr>
-                        <Td>{formData.name}</Td>
-                        <Td>{formData.birthday}</Td>
-                        <Td>{formData.relationship}</Td>
-                    </Tr>
-                </Tbody>
-            </Table>
-        </TableContainer>
+        <Box position="absolute" top={160} left={300} width={900} height={426}
+        borderRadius="2xl" padding="8px">
+            <TableContainer position="absolute" top={100}  width={850} height={330} padding="8px 3px" >
+                <Table variant="simple" fontSize="14px">
+                    <Thead>
+                        <Tr>
+                            <Th>Número de cédula</Th>
+                            <Th>Nombre</Th>
+                            <Th>Fecha de nacimiento</Th>
+                            <Th>Parentesco</Th>
+                            <Th>Acciones</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {familiares.map((familiar, index) => (
+                            <Tr key={index}>
+                                <Td>{familiar.ci}</Td>
+                                <Td>{familiar.name}</Td>
+                                <Td>{familiar.birthday}</Td>
+                                <Td>{familiar.relationship}</Td>
+                                <Td>
+                                    <EditIcon mr={2} cursor="pointer" />
+                                    <DeleteIcon cursor="pointer" />
+                                </Td> 
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 };
+
