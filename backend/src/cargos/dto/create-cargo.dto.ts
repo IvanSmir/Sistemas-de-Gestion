26effcc0b8d9ab98a522cf7975fe682d9b38310f
José Ferreira <jf561428@gmail.com>
2024-05-14 02:14:52 -0400

    chore: Remove unused cargo-related files

--- backend/src/cargos/dto/create-cargo.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/cargos/dto/create-cargo.dto.ts
-import { Cargo } from '../entities/cargo.entity';
-import { OmitType } from '@nestjs/mapped-types';
-
-export class CreateCargoDto extends OmitType(Cargo, ['id'] as const) {}
--- backend/src/cargos/dto/update-cargo.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/cargos/dto/update-cargo.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCargoDto } from './create-cargo.dto';
-
-export class UpdateCargoDto extends PartialType(CreateCargoDto) {}
