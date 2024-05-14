50cd6ab1968c5a028eb3dd8bc637ee2d18d36b04
José Ferreira <jf561428@gmail.com>
2024-05-14 02:15:21 -0400

    chore: Add global prefix 'api' and Swagger documentation to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.use(cookieParser());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
     }),
   );
-  app.enableCors({
-    origin: ['http://localhost:3000'],  
-  });
-  await app.listen(3002);
+
+  const config = new DocumentBuilder()
+    .setTitle('Management Systems API for HR and Payroll')
+    .setDescription(
+      'API for managing employee data and payroll operations within organizational management systems. This API provides endpoints for handling personal details, employment records, and payroll processing, ensuring seamless integration and efficient management of human resources.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
