91523a841fe62aa05a60cbe3d452f3843ea1fe82
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-05-16 12:28:34 -0400

    react-hook-form en login

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,15 +1,15 @@
 'use client'
-import { useForm } from 'react-hook-form'
 import React, { useState } from 'react'
 import { Text, FormControl, Input, FormLabel, Select, Button, Stepper, Box, Switch, InputGroup, InputRightElement } from '@chakra-ui/react'
-
+import { useForm } from "react-hook-form";
 export const Login = () => {
     const [show, setShow] = useState(false)
 
     const handleClick = () => setShow(!show)
+    const { register, handleSubmit } = useForm()
     return (
         <Box boxShadow='base' p='6' rounded='md' bg='white'>
-            <form className='w-80' >
+            <form className='w-80' onSubmit={handleSubmit(data => { console.log((data)) })} >
                 <FormControl className='flex  justify-center flex-col gap-3'>
                     <Text fontSize='xl' as='b'>Iniciar sesion</Text>
                     <Text fontSize='sm'>Inicia sesion con tu correo y contraseña</Text>
@@ -18,8 +18,8 @@ export const Login = () => {
                             <Input
                                 type="email"
                                 id='email'
-                                name='email'
                                 placeholder="example@gmail.com"
+                                {...register('email', { required: true })}
                             />
 
                         </FormLabel>
@@ -31,11 +31,10 @@ export const Login = () => {
                             <InputGroup size='md'>
                                 <Input
                                     id='password'
-                                    name='password'
                                     pr='4.5rem'
                                     type={show ? 'text' : 'password'}
                                     placeholder='Enter password'
-
+                                    {...register('password', { required: true })}
                                 />
                                 <InputRightElement width='4.5rem'>
                                     <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -47,7 +46,7 @@ export const Login = () => {
                     </div>
 
                     <a href="#">Olvidaste tu contraseña?</a>
-                    <Button bg='#AA546D' color='white' size='md'>Iniciar</Button>
+                    <Button bg='#AA546D' color='white' size='md' type='submit'>Iniciar</Button>
 
 
                 </FormControl>
