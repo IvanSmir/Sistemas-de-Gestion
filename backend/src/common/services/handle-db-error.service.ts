baaccb24a1abf6035956d867f5ae96bc1ff0c69f
José Ferreira <jf561428@gmail.com>
2024-05-15 21:59:59 -0400

    feat: Improve error handling in HandleDbErrorService
    The HandleDbErrorService has been updated to improve error handling. Specifically, the code now uses a logger to log the error instead of directly printing it to the console. Additionally, the service now handles specific PrismaClientKnownRequestError codes and throws appropriate exceptions based on the error type. This ensures that the error messages are more descriptive and user-friendly.

--- backend/src/common/services/handle-db-error.service.ts
+++ backend/src/common/services/handle-db-error.service.ts
@@ -2,22 +2,36 @@ import {
   Injectable,
   BadRequestException,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class HandleDbErrorService {
+  private readonly logger = new Logger(HandleDbErrorService.name);
+
   handleDbError(e: any, type: string, value: string) {
-    console.error(e);
-    if (e.code === 'P2002') {
-      throw new BadRequestException(`${type} with ${value} already exists`);
+    this.logger.error(e);
+
+    if (e instanceof PrismaClientKnownRequestError) {
+      switch (e.code) {
+        case 'P2002':
+          throw new BadRequestException(`${type} with ${value} already exists`);
+        case 'P2003':
+          const match = e.message.match(
+            /Foreign key constraint failed on the field: `(.+?)`/,
+          );
+          const field = match ? match[1] : 'unknown field';
+          throw new BadRequestException(`The id  for ${field} does not exist.`);
+        case 'P2025':
+          throw new BadRequestException(`The provided ${type} does not exist`);
+        default:
+          throw new InternalServerErrorException('Something went wrong');
+      }
+    } else if (e.code?.startsWith('5')) {
+      throw new InternalServerErrorException('Something went wrong');
     }
 
-    if (e.code === 'P2025') {
-      throw new BadRequestException(`The provided ${type} does not exist)`);
-    }
-
-    throw new InternalServerErrorException(
-      'An error occurred while processing the request, please try again later.',
-    );
+    throw e;
   }
 }
