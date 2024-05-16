bf6ad346c68fecee4c5eddacc29ed8825e6c147e
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-05-16 11:15:42 -0400

    Agregue los tipos

---
+++ client/src/types/employee.ts
@@ -0,0 +1,12 @@
+interface Employee {
+    name: string;
+    email: string;
+    image: string;
+    gender: string;
+    address: string;
+    ruc: string;
+    joinDate: Date;
+    birthdate: Date;
+    phone: string;
+}
+export default Employee;
---
+++ client/src/types/relative.ts
@@ -0,0 +1,12 @@
+interface Relative {
+    name: string;
+    last_name: string;
+    address: string;
+    phone: string;
+    email: string;
+    ci: string;
+    birthDate: Date;
+    gender: string;
+    relationshipType: string;
+}
+export default Relative;
