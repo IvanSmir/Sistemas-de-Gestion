437e5ce73c9c023669638c24a52a51fba0c1f73a
Ivan <jf561428@gmail.com>
2024-05-24 17:10:31 -0400

    change to id

---
+++ client/src/app/(dashboard)/employees/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { EmployeeDetails } from '@/components/employees/details/details'
+const detailsPage = () => {
+    return (
+        <div>
+            <EmployeeDetails></EmployeeDetails>
+        </div>
+    )
+}
+export default detailsPage
