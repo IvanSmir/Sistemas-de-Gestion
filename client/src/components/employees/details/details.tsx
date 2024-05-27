0e8f4c14955d95ba9fd9aa7ee61d7d555f0bc38b
José Ferreira <jf561428@gmail.com>
2024-05-26 23:48:09 -0400

    chore: Refactor employee module to improve code organization and readability

--- client/src/components/employees/details/details.tsx
+++ client/src/components/employees/details/details.tsx
@@ -7,15 +7,14 @@ import router from 'next/router'
 import { useParams, useRouter } from 'next/navigation'
 import { getEmployeeId, updateEmployee } from '@/utils/detail.http'
 import { useAuth } from '@/components/context/AuthProvider'
-import employee from '@/types/employee'
-import positionEmployee from '@/types/positionEmployee'
+
 
 export const EmployeeDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const toast = useToast();
     const auth = useAuth();
     console.log(id)
-    
+
 
     const [formData, setFormData] = useState(
         {
@@ -27,7 +26,7 @@ export const EmployeeDetails = () => {
             phone: "",
             ruc: "",
             joinDate: new Date(),
-            birthdate: new Date(),
+            birthDate: new Date(),
             cargo: "",
             detalle: "",
             sueldoBase: ""
@@ -35,34 +34,35 @@ export const EmployeeDetails = () => {
     );
 
     const [isEditing, setIsEditing] = useState(false);
-    
-    useEffect(()=>{
 
-        const fetchEmployee = async()=>{
-            try{
-                const {user} = auth;
-                if(user?.token){
-                const token = user.token; 
-                const employeeId = typeof id === "string" ? id : ""
-                const data = await getEmployeeId(employeeId,token);
-                setFormData({
-                    name: data.name || "",
-                    email: data.email || "",
-                    image: data.image || "",
-                    gender: data.gender || "",
-                    direction: data.direction || "",
-                    phone: data.phone || "",
-                    ruc: data.ruc || "",
-                    joinDate: data.joinDate ? new Date(data.joinDate) : new Date(),
-                    birthdate: data.birthdate ? new Date(data.birthdate) : new Date(),
-                    cargo: data.cargo || "",
-                    detalle: data.detalle || "",
-                    sueldoBase: data.sueldoBase || ""
+    useEffect(() => {
+
+        const fetchEmployee = async () => {
+            try {
+                const { user } = auth;
+                if (user?.token) {
+                    const token = user.token;
+                    const employeeId = typeof id === "string" ? id : ""
+                    const data = await getEmployeeId(employeeId, token);
+                    console.log('data', data);
+                    setFormData({
+                        name: data.person.name || "",
+                        email: data.person.email || "",
+                        image: data.person.image || "",
+                        gender: data.person.gender === 'male' ? 'Masculino' : 'Femenino',
+                        direction: data.person.address || "",
+                        phone: data.person.phone || "",
+                        ruc: data.person.ciRuc || "",
+                        joinDate: data.enterDate ? new Date(data.enterDate) : new Date(),
+                        birthDate: data.person.birthDate ? new Date(data.person.birthDate) : new Date(),
+                        cargo: data.cargo || "",
+                        detalle: data.person.detalle || "",
+                        sueldoBase: data.person.sueldoBase || ""
                     });
-                }else {
+                } else {
                     throw new Error('Token is missing');
                 }
-            }catch (error) {
+            } catch (error) {
                 const err = error as Error;
                 toast({
                     title: "Error",
@@ -102,7 +102,7 @@ export const EmployeeDetails = () => {
             const { user } = auth;
             const token = user?.token || '';
             const employeeId = typeof id === "string" ? id : ""
-            const employeeResponse = await updateEmployee(employeeId,employeeData, token);
+            const employeeResponse = await updateEmployee(employeeId, employeeData, token);
             alert('Employee saved successfully!');
             router.push('/employees');
             console.log(employeeResponse);
@@ -113,39 +113,39 @@ export const EmployeeDetails = () => {
     };
     return (
         <>
-        <Box  p='10'> 
-            <Flex>
-                <Link href="/funcionarios">
+            <Box p='10'>
+                <Flex>
+                    <Link href="/employees">
+                        <Button
+                        >
+                            Volver
+                        </Button>
+                    </Link>
                     <Button
                     >
-                        Volver
+                        Editar
                     </Button>
-                </Link>
-                <Button
-                >
-                    Editar
-                </Button>
-                <Button
-                    onClick={handleEdit}
-                >
-                    Guardar
-                </Button>
-                <Button
-                    onClick={handleCancel}
-                    disabled={!isEditing}
-                >
-                    Cancelar
-                </Button>
-            </Flex>
-            <Flex  p='10'>
+                    <Button
+                        onClick={handleEdit}
+                    >
+                        Guardar
+                    </Button>
+                    <Button
+                        onClick={handleCancel}
+                        disabled={!isEditing}
+                    >
+                        Cancelar
+                    </Button>
+                </Flex>
+                <Flex p='10'>
                     <form>
                         <FormControl >
                             <Flex direction={{ base: "column", md: "row" }} >
                                 <Box p='10'>
-                                    
+
                                     <Wrap>
                                         <WrapItem>
-                                            <Avatar size='3xl'  />
+                                            <Avatar size='3xl' />
                                         </WrapItem>
                                     </Wrap>
 
@@ -156,7 +156,7 @@ export const EmployeeDetails = () => {
                                         <Box mb={8}>
                                             <FormLabel htmlFor="ruc">Ruc/Ci:</FormLabel>
                                             <Input
-                                                
+
                                                 value={formData.ruc !== undefined ? formData.ruc : ''}
                                                 readOnly={!isEditing}
                                                 mb={4}
@@ -164,7 +164,7 @@ export const EmployeeDetails = () => {
                                             />
                                             <FormLabel htmlFor="gender">Sexo:</FormLabel>
                                             <Input
-                                                
+
                                                 value={formData.gender !== undefined ? formData.gender : ''}
                                                 readOnly={!isEditing}
                                                 mb={4}
@@ -173,7 +173,7 @@ export const EmployeeDetails = () => {
 
                                             <FormLabel htmlFor="cargo">Cargo:</FormLabel>
                                             <Input
-                                            
+
                                                 value={formData.cargo !== undefined ? formData.cargo : ''}
                                                 mb={4}
                                                 readOnly={!isEditing}
@@ -184,7 +184,7 @@ export const EmployeeDetails = () => {
                                         <Box mb={8}>
                                             <FormLabel htmlFor="name">Nombre:</FormLabel>
                                             <Input
-                                                
+
                                                 value={formData.name !== undefined ? formData.name : ''}
                                                 mb={4}
                                                 readOnly={!isEditing}
@@ -192,24 +192,23 @@ export const EmployeeDetails = () => {
                                             />
                                             <FormLabel htmlFor="birthdate">Fecha Nacimiento:</FormLabel>
                                             <Input
-                                                value={formData.birthdate !== undefined ? formData.birthdate.toLocaleDateString() : ''}
+                                                value={formData.birthDate !== undefined ? formData.birthDate.toLocaleDateString() : ''}
                                                 readOnly={!isEditing}
                                                 mb={4}
-                                                
                                             />
                                         </Box>
 
                                         <Box>
                                             <FormLabel htmlFor="email">Correo:</FormLabel>
                                             <Input
-                                                
+
                                                 value={formData.email !== undefined ? formData.email : ''}
                                                 readOnly={!isEditing}
                                                 mb={4}
                                             />
                                             <FormLabel htmlFor="direction">Dirección:</FormLabel>
                                             <Input
-                                                
+
                                                 value={formData.direction !== undefined ? formData.direction : ''}
                                                 readOnly={!isEditing}
                                             />
@@ -233,7 +232,6 @@ export const EmployeeDetails = () => {
                                                         <FormLabel htmlFor="joinDate">Fecha Ingreso:</FormLabel>
                                                         <Input
                                                             type="date"
-                                                            
                                                             value={formData.joinDate !== undefined ? formData.joinDate.toLocaleDateString() : ''}
                                                             readOnly={!isEditing}
                                                         />
@@ -241,7 +239,7 @@ export const EmployeeDetails = () => {
                                                     <Box mr={8}>
                                                         <FormLabel htmlFor="detalle">Detalle:</FormLabel>
                                                         <Input
-                                                            
+
                                                             value={formData.detalle !== undefined ? formData.detalle : ''}
                                                             readOnly={!isEditing}
                                                         />
@@ -249,7 +247,7 @@ export const EmployeeDetails = () => {
                                                     <Box>
                                                         <FormLabel htmlFor="sueldoBase">Sueldo Base:</FormLabel>
                                                         <Input
-                                                            
+
                                                             value={formData.sueldoBase !== undefined ? formData.sueldoBase : ''}
                                                             readOnly={!isEditing}
                                                         />
