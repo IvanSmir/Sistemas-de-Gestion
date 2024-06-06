d788faef2620964ac3be8571655f6c93c15aa43f
Luciana <lucianascheid@fiuni.edu.py>
2024-06-06 11:15:39 -0400

    mas nuevo

--- client/src/utils/detail.http.ts
+++ client/src/utils/detail.http.ts
@@ -48,6 +48,7 @@ export const updateEmployee = async (
   }
 };
 
+
 export const getEmployeeDetails = async (id: string, token: string) => {
   try {
     const response = await fetch(`${API_URL}/employee-details/employee/${id}`, {
@@ -68,6 +69,29 @@ export const getEmployeeDetails = async (id: string, token: string) => {
     throw new Error((error as Error).message || "Error al obtener empleado");
   }
 };
+
+
+export const createPositionDetails = async (data: any, token: string) => {
+  try {
+    const response = await fetch(`${API_URL}/employee-details/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data), 
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al guardar cargo");
+    }
+
+    return response.json();
+  } catch (error: any) {
+    throw new Error((error as Error).message || "Error al guardar cargo");
+  }
+};
+
 
 export const getEmployeeIncomeDetails = async (id: string, token: string) => {
   try {
@@ -111,5 +135,49 @@ export const getEmployeeExpenseDetails = async (id: string, token: string) => {
     throw new Error(
       (error as Error).message || "Error al obtener egresos del empleado"
     );
+  }
+};
+
+export const updatePositionDetails = async (id: string, positionDetailsData: any, token:string)=>{
+  try {
+    console.log("Token usado para crear:", token);
+    const response = await fetch(`${API_URL}/employee-details/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(positionDetailsData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al editar cargo");
+    }
+
+    return response.json();
+  } catch (error: any) {
+    throw new Error(
+      (error as Error).message || "Error al guardar cargo editado"
+    );
+  }
+}
+
+export const deletePositionDetail = async(id: string, token: string)=>{
+  try{
+    const response = await fetch(`${API_URL}/employee-details/${id}`,{
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      }
+    });
+    if (!response.ok) {
+      throw new Error("Error al borrar posiciones");
+    }
+  
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    throw new Error((error as Error).message || "Error al obtener posiciones");
   }
 };
