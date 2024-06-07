1161bdd35f1ff8cde6a6b11d3fb8136f96886bcc
José Ferreira <jf561428@gmail.com>
2024-06-07 09:57:40 -0400

    refactor: Update positionSchema to handle optional endDate and transform it into a Date object

--- client/src/validations/positionSchema.ts
+++ client/src/validations/positionSchema.ts
@@ -1,33 +1,40 @@
 import { z } from "zod";
 
-export const positionSchema = z.object({
-  positionId: z.string().min(1, { message: "Posición es requerida" }),
-  salaryType: z.enum(["minimum", "base"], {
-    message: "Tipo de sueldo es requerido",
-  }),
-  amount: z.preprocess(
-    (val) => Number(val),
-    z.number().positive("El salario debe ser un número positivo")
-  ),
-
-  endDate: z
-    .union([
-      z
-        .string()
-        .transform((val) => new Date(val))
-        .optional(),
-      z.date().optional(),
-    ])
-    .refine((date) => date === undefined || !isNaN(date.getTime()), {
-      message: "Invalid date",
+export const positionSchema = z
+  .object({
+    positionId: z.string().min(1, { message: "Posición es requerida" }),
+    salaryType: z.enum(["minimum", "base"], {
+      message: "Tipo de sueldo es requerido",
     }),
-
-  startDate: z
-    .preprocess(
-      (arg) => new Date(arg as string),
-      z
-        .date()
-        .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" })
-    )
-    .optional(),
-});
+    amount: z.preprocess(
+      (val) => Number(val),
+      z.number().positive("El salario debe ser un número positivo")
+    ),
+    startDate: z
+      .preprocess(
+        (arg) => new Date(arg as string),
+        z
+          .date()
+          .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" })
+      )
+      .optional(),
+    endDate: z
+      .union([
+        z
+          .string()
+          .transform((val) => new Date(val))
+          .optional(),
+        z.date().optional(),
+      ])
+      .refine((date) => date === undefined || !isNaN(date.getTime()), {
+        message: "Invalid date",
+      }),
+  })
+  .refine(
+    (data) =>
+      !data.endDate || !data.startDate || data.endDate >= data.startDate,
+    {
+      message: "Fecha de inicio debe ser menor que la fecha de fin",
+      path: ["endDate"],
+    }
+  );
