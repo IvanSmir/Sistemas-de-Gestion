549f7387c3fd22dbfb28d1ec03bc3424303b5543
José Ferreira <jf561428@gmail.com>
2024-06-06 22:47:17 -0400

    refactor: Update employeeDetailsSchema and positionSchema with date preprocessing for startDate and endDate

--- client/src/validations/formSchema.ts
+++ client/src/validations/formSchema.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    name: z
-        .string()
-        .min(1, { message: "El cargo es requerido" })
-        .min(3, { message: "El cargo debe de ser minimo de 3 caracteres" })
-        .max(25, { message: "El cargo no puede superar los 25 caracteres" })
-        .regex(/^[a-zA-Z ]*$/, "El cargo solo puede contener letras"),
+  name: z
+    .string()
+    .min(1, { message: "El cargo es requerido" })
+    .min(3, { message: "El cargo debe de ser minimo de 3 caracteres" })
+    .max(25, { message: "El cargo no puede superar los 25 caracteres" })
+    .regex(/^[a-zA-Z ]*$/, "El cargo solo puede contener letras"),
 
-    description: z
-        .string()
-        .max(50, { message: "La descripcion no puede superar los 50 caracteres" }),
+  description: z
+    .string()
+    .max(50, { message: "La descripcion no puede superar los 50 caracteres" }),
 });
