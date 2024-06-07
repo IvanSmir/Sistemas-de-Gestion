47be10e84488c431f2c7fd2e47a189f4593be173
José Ferreira <jf561428@gmail.com>
2024-06-06 22:46:40 -0400

    refactor: Update employeeDetailsSchema with date preprocessing for startDate and endDate

--- client/src/validations/employeeDetailsSchema.ts
+++ client/src/validations/employeeDetailsSchema.ts
@@ -1,10 +1,31 @@
+import { start } from "repl";
 import { z } from "zod";
 
 export const employeeDetailsSchema = z.object({
-  employeeId: z.string().uuid({ message: "El ID del empleado debe ser un UUID válido" }),
-  positionId: z.string().uuid({ message: "El ID de la posición debe ser un UUID válido" }),
-  startDate: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, { message: "El formato de la fecha de inicio es inválido" }),
-  salaryType: z.enum(["minimum", "base"], { message: "El tipo de salario debe ser 'minimum' o 'base'" }),
-  salary: z.number().positive({ message: "El salario debe ser un número positivo" }),
-  endDate: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, { message: "El formato de la fecha de fin es inválido" }),
+  employeeId: z
+    .string()
+    .uuid({ message: "El ID del empleado debe ser un UUID válido" }),
+  positionId: z
+    .string()
+    .uuid({ message: "El ID de la posición debe ser un UUID válido" }),
+  endDate: z
+    .preprocess(
+      (arg) => new Date(arg as string),
+      z
+        .date()
+        .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" })
+    )
+    .optional(),
+  startDate: z.preprocess(
+    (arg) => new Date(arg as string),
+    z
+      .date()
+      .refine((date) => !isNaN(date.getTime()), { message: "Invalid date" })
+  ),
+  salaryType: z.enum(["minimum", "base"], {
+    message: "El tipo de salario debe ser 'minimum' o 'base'",
+  }),
+  salary: z
+    .number()
+    .positive({ message: "El salario debe ser un número positivo" }),
 });
