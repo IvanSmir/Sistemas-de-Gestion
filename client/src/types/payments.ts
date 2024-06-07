554a45d3f6c72968a80d5e27291e956326419daf
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-06-06 23:39:59 -0400

    cambios

---
+++ client/src/types/payments.ts
@@ -0,0 +1,14 @@
+
+export interface Item {
+  id: string
+  payrollDetailId: string
+  isIncome: boolean
+  amount: number
+  description: string
+  createdAt: string
+  updatedAt: string
+  userId: string
+  isDeleted: boolean
+  isIps: boolean
+  isBonification: boolean
+}
