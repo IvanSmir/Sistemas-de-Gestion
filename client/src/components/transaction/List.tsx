045d040a25ae7353106779152a95772a833d5d49
Gessica <adarogessica@gmail.com>
2024-06-28 00:40:44 -0400

    filtros para movimientos

--- client/src/components/transaction/List.tsx
+++ client/src/components/transaction/List.tsx
@@ -1,8 +1,10 @@
 'use client'
-import { TableContainer, Table, Tbody, Td, Th, Thead, Tr, Box, Flex, Select, Button, Input, Heading } from '@chakra-ui/react';
-import React, { ChangeEvent, useState } from 'react'
+import { TableContainer, Table, Tbody, Td, Th, Thead, Tr, Box, Flex, Select, Button, Input, Heading, FormControl, FormLabel } from '@chakra-ui/react';
+import React, { ChangeEvent, useState, useEffect } from 'react'
 import Pagination from '../lists/Pagination';
 import Link from 'next/link';
+import { getEmployeeByTerm } from '@/utils/employee.http';
+import { useAuth } from "../context/AuthProvider";
 
 // Definimos los tipos para las props
 type TableEmployeeProps = {
@@ -17,21 +19,70 @@ export const List: React.FC<TableEmployeeProps> = ({ data, columnMapping, setEmp
 
     const headers = Object.keys(columnMapping);
     console.log(data);
-    const [filters, setFilters] = useState<{ ci: string; ageRange: string }>({
-        ci: '',
-        ageRange: '',
-    });
-    const handleCiFilter = (e: ChangeEvent<HTMLInputElement>) => {
-        setFilters({ ...filters, ci: e.target.value });
+    const auth = useAuth();
+    
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState(data);
+    const [isSearching, setIsSearching] = useState(false);
+    
+    const handleTermFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+        
+        if (term.length >= 3) {
+            setIsSearching(true);
+            const localFiltered = data.filter((datum) => {
+                return Object.keys(columnMapping).some((key) => {
+                    const value = datum.person[columnMapping[key]]?.toString().toLowerCase();
+                    return value && value.includes(term.toLowerCase());
+                });
+            });
+            setFilteredData(localFiltered);
+        } else {
+            setFilteredData(data);
+            setIsSearching(false);
+        }
     };
-
-    const handleAgeRangeFilter = (e: ChangeEvent<HTMLSelectElement>) => {
-        setFilters({ ...filters, ageRange: e.target.value });
-    };
+    
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(async () => {
+            if (searchTerm && searchTerm.length >= 3) {
+                try {
+                    const { user } = auth;
+                    const token = user?.token || '';
+                    const result = await getEmployeeByTerm(searchTerm, token);
+                    setFilteredData(Array.isArray(result) ? result : [result]);
+                } catch (error) {
+                    console.error("Error al obtener empleados:", error);
+                    // Mantenemos los resultados locales en caso de error
+                }
+            }
+        }, 300);
+    
+        return () => clearTimeout(delayDebounceFn);
+    }, [searchTerm, auth]);
+    
+    useEffect(() => {
+        if (!isSearching) {
+            setFilteredData(data);
+        }
+    }, [data, isSearching]);
 
     return (
         <Flex width={"90%"} flexDirection={"column"}>
             <Heading color={"gray.600"} mt={4} marginLeft={5} width={"100%"}>Ingresos</Heading>
+
+            <FormControl id="filter" width={{ base: "55%", sm: "50%", md: "40%", lg: "20%" }} >
+            <FormLabel fontSize="sm">Buscar por Nombre</FormLabel>
+                    <Input
+                        value={searchTerm}
+                        onChange={handleTermFilter}
+                        size="sm" rounded={15}
+                        background='white'
+                        color='gray.600'
+                        _hover={{ bg: "gray.100" }}
+                    />
+                </FormControl>
 
             <Box backgroundColor={'white'} top={160} left={300} width={"100%"} height={426} borderRadius="2xl" padding="8px" mt={10} >
                 <TableContainer>
@@ -46,13 +97,13 @@ export const List: React.FC<TableEmployeeProps> = ({ data, columnMapping, setEmp
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {data.map((datum) => (
-                                <Tr key={datum.id}>
-                                    {headers.map((header) => (
-                                        <Td key={`${datum.id}-${header}`} textAlign="center" verticalAlign="middle">
-                                            {datum.person[columnMapping[header]]}
-                                        </Td>
-                                    ))}
+                        {filteredData.map((datum) => (
+                                    <Tr key={datum.id}>
+                                        {headers.map((header) => (
+                                            <Td key={`${datum.id}-${header}`} textAlign="center" verticalAlign="middle">
+                                                {datum.person[columnMapping[header]]}
+                                            </Td>
+                                        ))}
                                     <Td>
                                         <Link className='text-blue-500' href={`/transaction/incomes/${datum.id}`}>
                                             Ingresos
@@ -64,7 +115,9 @@ export const List: React.FC<TableEmployeeProps> = ({ data, columnMapping, setEmp
                     </Table>
                 </TableContainer>
             </Box>
+            {!isSearching && (
             <Pagination setEmployeedata={setEmployeeData} total={total} />
+        )}
 
         </Flex >
     );
--- client/src/components/transaction/ListEmployee.tsx
+++ client/src/components/transaction/ListEmployee.tsx
@@ -1,9 +1,10 @@
 'use client'
-import { TableContainer, Table, Tbody, Td, Th, Thead, Tr, Box, Flex, Select, Button, Input, Heading } from '@chakra-ui/react';
-import React, { ChangeEvent, useState } from 'react'
+import { TableContainer, Table, Tbody, Td, Th, Thead, Tr, Box, Flex, Select, Button, Input, Heading, FormControl, FormLabel } from '@chakra-ui/react';
+import React, { ChangeEvent, useState, useEffect } from 'react'
 import Pagination from '../lists/Pagination';
 import Link from 'next/link';
-
+import { useAuth } from "../context/AuthProvider";
+import { getEmployeeByTerm } from '@/utils/employee.http';
 
 type TableEmployeeProps = {
     data: { id: string;[key: string]: any }[]; // Cada dato tiene un ID y valores dinámicos
@@ -16,9 +17,68 @@ export const ListEmployee: React.FC<TableEmployeeProps> = ({ data, columnMapping
     // Obtener los encabezados desde las claves de columnMapping
     const headers = Object.keys(columnMapping);
     console.log(data);
+    const auth = useAuth();
+    
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState(data);
+    const [isSearching, setIsSearching] = useState(false);
+    
+    const handleTermFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+        
+        if (term.length >= 3) {
+            setIsSearching(true);
+            // Búsqueda local inmediata
+            const localFiltered = data.filter((datum) => {
+                return Object.keys(columnMapping).some((key) => {
+                    const value = datum.person[columnMapping[key]]?.toString().toLowerCase();
+                    return value && value.includes(term.toLowerCase());
+                });
+            });
+            setFilteredData(localFiltered);
+        } else {
+            setFilteredData(data);
+            setIsSearching(false);
+        }
+    };
+    
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(async () => {
+            if (searchTerm && searchTerm.length >= 3) {
+                try {
+                    const { user } = auth;
+                    const token = user?.token || '';
+                    const result = await getEmployeeByTerm(searchTerm, token);
+                    setFilteredData(Array.isArray(result) ? result : [result]);
+                } catch (error) {
+                    console.error("Error al obtener empleados:", error);
+                }
+            }
+        }, 300);
+    
+        return () => clearTimeout(delayDebounceFn);
+    }, [searchTerm, auth]);
+    
+    useEffect(() => {
+        if (!isSearching) {
+            setFilteredData(data);
+        }
+    }, [data, isSearching]);
     return (
         <Flex width={"90%"} flexDirection={"column"}>
             <Heading color={"gray.600"} mt={4} marginLeft={5} width={"100%"}>Egresos</Heading>
+            <FormControl id="filter" width={{ base: "55%", sm: "50%", md: "40%", lg: "20%" }} >
+            <FormLabel fontSize="sm">Buscar por Nombre</FormLabel>
+                    <Input
+                        value={searchTerm}
+                        onChange={handleTermFilter}
+                        size="sm" rounded={15}
+                        background='white'
+                        color='gray.600'
+                        _hover={{ bg: "gray.100" }}
+                    />
+                </FormControl>
 
             <Box backgroundColor={'white'} top={160} left={300} width={"100%"} height={426} borderRadius="2xl" padding="8px" mt={10} >
                 <TableContainer>
@@ -33,13 +93,13 @@ export const ListEmployee: React.FC<TableEmployeeProps> = ({ data, columnMapping
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {data.map((datum) => (
-                                <Tr key={datum.id}>
-                                    {headers.map((header) => (
-                                        <Td key={`${datum.id}-${header}`} textAlign="center" verticalAlign="middle">
-                                            {datum.person[columnMapping[header]]}
-                                        </Td>
-                                    ))}
+                        {filteredData.map((datum) => (
+                                    <Tr key={datum.id}>
+                                        {headers.map((header) => (
+                                            <Td key={`${datum.id}-${header}`} textAlign="center" verticalAlign="middle">
+                                                {datum.person[columnMapping[header]]}
+                                            </Td>
+                                        ))}
                                     <Td>
                                         <Link className='text-blue-500' href={`/transaction/expenses/${datum.id}`}>
                                             Egresos
@@ -51,7 +111,9 @@ export const ListEmployee: React.FC<TableEmployeeProps> = ({ data, columnMapping
                     </Table>
                 </TableContainer>
             </Box>
+            {!isSearching && (
             <Pagination setEmployeedata={setEmployeeData} total={total} />
+        )}
 
 
         </Flex >
