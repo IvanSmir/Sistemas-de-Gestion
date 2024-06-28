aeacb1bb4673ff5e3d720549abee3b5d766afdaa
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2024-06-27 20:24:04 -0400

    modificado el pdf

--- client/src/components/general/report/ReportModal.tsx
+++ client/src/components/general/report/ReportModal.tsx
@@ -5,6 +5,7 @@ import {
 } from '@chakra-ui/react';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import ReportPDF from './ReportPDF';
+import { Income } from '../incomes/income';
 
 const ReportModal = ({ isOpen, onClose, reportData }) => {
 
@@ -16,7 +17,9 @@ const ReportModal = ({ isOpen, onClose, reportData }) => {
     const [ipsArray, setIpsArray] = useState([])
     const [familyArray, setFamilyArray] = useState([])
     const [incomeArray, setIncomeArray] = useState([])
+    const [expenseArray, setExpenseArray] = useState([])
     const [totalArray, setTotalArray] = useState([])
+    const [salaryArray, setSalaryArray] = useState([])
 
     const handleSelectChange = (event) => {
         setSelectedReport(event.target.value);
@@ -24,41 +27,63 @@ const ReportModal = ({ isOpen, onClose, reportData }) => {
 
     useEffect(() => {
         if (reportData.length > 0) {
-            let ipsArray_= [], familyArray_ = [], incomeArray_ = [], totalArray_ = [];
-            let ips_: number = 0, family_: number = 0, income_: number = 0, total_: number = 0;
+            let ipsArray_ = [], familyArray_ = [], incomeArray_ = [], totalArray_ = [], salaryArray_ = [], expenseArray_=[];
+            let ips_: number = 0, family_: number = 0, income_: number = 0, total_: number = 0, salary_: number = 0;
             reportData.forEach(d => {
+                salary_ = 0;
                 d.items.forEach(i => {
                     console.log(i)
-                    totalArray_ = [...totalArray_, {name: d.name, cin: d.cin, ...i}]
+                    totalArray_ = [...totalArray_, { name: d.name, cin: d.cin, ...i }]
                     if (i.isBonification) {
                         console.log("bonificacion")
-                        familyArray_ = [...familyArray_, {name: d.name, cin: d.cin, ...i}]
+                        familyArray_ = [...familyArray_, { name: d.name, cin: d.cin, ...i }]
                         family_ += Number(i.amount)
                         total_ += Number(i.amount)
                     } else {
                         if (i.isIncome) {
                             console.log("income")
-                            incomeArray_ = [...incomeArray_, {name: d.name, cin: d.cin, ...i}]
-                            income_ += Number(i.amount)
-                            total_ += Number(i.amount)
+                            if (i.description.includes("Salario")) {
+                                salary_ += Number(i.amount)
+                                total_ += Number(i.amount)
+                            } else {
+                                incomeArray_ = [...incomeArray_, { name: d.name, cin: d.cin, ...i }]
+                                income_ += Number(i.amount)
+                                total_ += Number(i.amount)
+                            }
                         } else {
                             if (i.isIps) {
                                 console.log("ips")
-                                ipsArray_ = [...ipsArray_, {name: d.name, cin: d.cin, ...i}]
+                                ipsArray_ = [...ipsArray_, { name: d.name, cin: d.cin, ...i }]
                                 ips_ += Number(i.amount)
-                                total_ += Number(i.amount)
+                                salary_ -= Number(i.amount)
+                            }else{
+                                expenseArray_ = [...expenseArray_, { name: d.name, cin: d.cin, ...i }]
+                                salary_ -= Number(i.amount)
+                                total_ -= Number(i.amount)
                             }
                         }
                     }
 
                 });
+                salaryArray_ = [...salaryArray_, {
+                    name: d.name, cin: d.cin,
+                    amount: salary_,
+                    description: "Salario",
+                    isBonification: false,
+                    isIncome: true,
+                    isIps: false,
+                }]
             });
+            console.log("incomes", incomeArray_)
+            console.log("salarios", salaryArray_)
             setIps(ips_)
             setFamily(family_)
-            setIncome(income_)
+            setIncome(income_ + salary_)
             setTotal(total_)
             setIpsArray(ipsArray_)
             setFamilyArray(familyArray_)
+            setExpenseArray(expenseArray_)
+            setSalaryArray(salaryArray_)
             setIncomeArray(incomeArray_)
             setTotalArray(totalArray_)
         }
@@ -88,21 +113,23 @@ const ReportModal = ({ isOpen, onClose, reportData }) => {
                 <ModalCloseButton />
                 <ModalBody>
                     <Select value={selectedReport} onChange={handleSelectChange} mb={4} borderColor="#e4b1bc" _focus={{ borderColor: "#e4b1bc" }} >
-                        <option  value='resumenIps'>Resumen de IPS</option>
+                        <option value='resumenIps'>Resumen de IPS</option>
                         <option value='resumenBonificacion'>Resumen de Bonificación</option>
                         <option value='resumenSalario'>Resumen de Salario</option>
                         <option value='resumenTotal'>Resumen Total</option>
                     </Select>
                     <Box>
-                        <pre>Total:{renderReportContent()}Gs.</pre>
+                   
+                       {/* <pre>Total: {renderReportContent()} Gs.</pre> */}
+                    
                     </Box>
                 </ModalBody>
                 <ModalFooter>
                     <Button colorScheme="gray" mr={3} onClick={onClose}>
                         Cerrar
                     </Button>
-                    <PDFDownloadLink 
-                        document={<ReportPDF reportData={reportData} selectedReport={selectedReport} ipsArray={ipsArray} totalArray={totalArray} familyArray={familyArray} incomeArray={incomeArray} />}
+                    <PDFDownloadLink
+                        document={<ReportPDF reportData={reportData} selectedReport={selectedReport} ipsArray={ipsArray} totalArray={totalArray} familyArray={familyArray} salaryArray={salaryArray} incomeArray={incomeArray} expenseArray={expenseArray} />}
                         fileName="informe_mensual.pdf"
                     >
                         {({ loading }) => (loading ? 'Cargando documento...' : 'Descargar PDF')}
